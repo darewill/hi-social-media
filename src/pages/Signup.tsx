a87022cd18334207b21e100343ff9ea98af0a635
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-09-08 21:51:21 +0200

    login & register buttons fully navigable

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,31 @@
 import '../assets/css/Signup.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/');
+  };
   return (
     <div className='signup'>
       <div className='signup-box'>
-          <div className="sign-text">
+          <div className="signup-text">
             <h1>Hi!</h1>
             <p>Sign up to see and upload photos.</p>
           </div>
-        <form className="sign-form">
-          <input type="text" className="sign-input" required placeholder="Full Name"/>
-          <input type="email" className="sign-input" required placeholder="Email"/>
-          <input type="text" className="sign-input" required placeholder="username"/>
-          <input type="password" className="sign-input" required placeholder="password"/>
+        <form className="signup-form">
+          <input type="text" className="signup-input" required placeholder="Full Name"/>
+          <input type="text" className="signup-input" required placeholder="Email"/>
+          <input type="text" className="signup-input" required placeholder="username"/>
+          <input type="password" className="signup-input" required placeholder="password"/>
           <button type="submit" className='btn'>Sign up</button>
         </form>
+      </div>
+      <hr className="hr-line"></hr>
+      <div className='signup-box'>
+        <h3>Already have an account?</h3>
+        <button className="btn" onClick={handleLoginClick}>Login</button>
       </div>
     </div>
   )
