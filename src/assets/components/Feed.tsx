3c9f0649f59ae03f57b8017ac3ead1a739435039
darewill <urimm.r@gmail.com>
2024-09-10 19:39:41 +0200

    Feed add

--- src/assets/components/Feed.tsx
+++ src/assets/components/Feed.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import '../css/Feeds.css'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faListDots, faStar, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 
-export default function Feed() {
+export default function Feed({fd}) {
   return (
-    <div>Feed</div>
+    <div className='feed'>
+      <div className="top-content">
+        <Link to='/profile/id'>
+          <div className="user">
+            <img src={fd.feedProfile} alt=''/>
+            <div>
+            <h5>{fd.name}</h5>
+            <small>1 Minute ago</small>
+            </div>
+          </div>
+        </Link>
+        <span><FontAwesomeIcon icon={faListDots}/></span>
+      </div>
+      <div className="mid-content">
+        <p>{fd.desc}</p>
+        <img src={fd.feedImage}/>
+      </div>
+      <div className="bottom-content">
+        <div className="action-item">
+          <span><FontAwesomeIcon icon={faStar}/> 14 Stars</span>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/assets/components/Feeds.tsx
+++ src/assets/components/Feeds.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import '../css/Feeds.css'
+import React from "react";
+import "../css/Feeds.css";
+import Feed from "./Feed";
+import HomeFeedData from '../js/HomeFeedData';
 
-export default function Feed() {
+export default function Feeds() {
   return (
-    <div>Feeds</div>
-  )
+    <div className='feeds'>
+      {
+        HomeFeedData.map(fd=>(
+          <Feed fd={fd} key={fd.key} />
+        ))
+      }
+    </div>
+  );
 }
