481ca067282ce8ee54cb8af884f5175f2a013e46
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-09-10 13:26:08 +0200

    Friend request add & customize

--- src/assets/components/FriendRequest.tsx
+++ src/assets/components/FriendRequest.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import '../css/FriendRequest.css'
-import { Link } from 'react-router-dom'
-import FriendReqData from '../js/FriendRequest'
+import React from "react";
+import "../css/FriendRequest.css";
+import { Link } from "react-router-dom";
+import FriendReqData from "../js/FriendRequest";
 
 export default function FriendRequest() {
   return (
-    <div className='Friend-requests'>
-        <h4>Friend Requests</h4>
-        {
-            FriendReqData.map(friend=>(
-                <div className="request">
-                    <Link to='/profile/id'>
-                    <div className="info" key={friend.id}>
-                        <img src={friend.img} alt={friend.name}/>
-                    </div>
-                    <div className="info-name">
-                        <h5>{friend.name}</h5>
-                        <p>{friend.info}</p>
-                    </div>
-                    </Link>
-                    <div className="action">
-                        <div className="btn btn-primary">Accept</div>
-                        <div className="btn btn-red">Decline</div>
-                    </div>
-                </div>
-            ))
-        }
+    <div className="Friend-requests">
+      <h4>Friend Requests</h4>
+      {FriendReqData.map((friend) => (
+        <div className="request">
+          <Link to="/profile/id">
+            <div className="info" key={friend.id}>
+              <div className="user">
+                <img src={friend.img} alt={friend.name} className='fr-img'/>
+                <h5>{friend.name}</h5>
+              </div>
+              <div className="info-name">
+                <p>{friend.info}</p>
+              </div>
+            </div>
+          </Link>
+          <div className="action">
+            <button className="btn btn-primary">Accept</button>
+            <button className="btn btn-red">Decline</button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
