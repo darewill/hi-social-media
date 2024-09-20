cf3d7b30073c3114652bd8a8326167c4c6714734
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-09-20 18:23:58 +0200

    Users name fix in chatbox

--- src/pages/Chatbox/Chatbox.tsx
+++ src/pages/Chatbox/Chatbox.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
 import "../../assets/css/Chatbox.css";
 import Stories from "../../assets/components/Stories";
 import CurrentUserData from "../../assets/dummyAPIs/CurrentUserData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleRight, faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import { auth } from "../../firebaseConfig";
+import ProfileImage from "../../assets/images/ezio.jpg";
 
 export default function Chatbox() {
+
+  const [photoURL, setPhotoURL] = useState<string | null>(null);
+  const [displayName, setDisplayName] = useState<string | null>(null);
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      setDisplayName(auth.currentUser.displayName);
+      setPhotoURL(auth.currentUser.photoURL);
+    }
+  }, []);
+
   return (
     <>
       <Stories />
       <div className="chat-box">
         <div className="chat-box-top">
-          <img src={CurrentUserData[0].ProfileImage} alt="" />
+          <img src={photoURL || ProfileImage} alt="" />
           <div className="user-name">
-            <h3>{CurrentUserData[0].name}</h3>
-            <h5>{CurrentUserData[0].username}</h5>
+            <h3>{user?.displayName || 'No Name Available'}</h3>
+            {/* <h5>{CurrentUserData[0].username}</h5> */}
           </div>
         </div>
         <div className="chat-box-bottom">
